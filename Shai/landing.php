157667855e04111be333abdd57ec1f61ef11cf23
shy142897 <103382424+shy142897@users.noreply.github.com>
2022-04-14 23:52:50 +0545

    Add files via upload

---
+++ Shai/landing.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Real Estate Property Site</title>
+<link href="at/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<header>
+<div class="main">
+<div class="logo">
+<img src="at/logo.png">
+</div>
+<ul>
+<li class="active"><a href="index.php">Home</a></li>
+<li><a href="at/buy.php">Buy</a></li>
+<li><a href="index.php">Gallery</a></li>
+<li><a href="at/contact.php">Contact</a></li>
+</div>
+<div class="title">
+<h1>     Nest Seekers</h1>
+</div>
+
+<div class="button">
+<a href="at/login.php" class="btn">LOG IN</a>
+<a href="at/signup.php" class="btn">SIGN UP</a>
+
+</header>
+</body>
+</html>
